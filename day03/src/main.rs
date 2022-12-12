9b803ce0c1db602a89d27ae173f5fb296990e7cf
Kis Imre <k.iminet@gmail.com>
2022-12-12 20:58:34 +0100

    Refactoring Day 03

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,6 +1,9 @@
+// Copyright (c) 2022, Kis Imre. All rights reserved.
+// SPDX-License-Identifier: MIT
+
 #![feature(iter_array_chunks)]
 
-fn get_value(c : char) -> u32 {
+fn get_value(c: char) -> u32 {
     if c.is_lowercase() {
         ((c as u8) - ('a' as u8) + 1) as u32
     } else {
@@ -8,30 +11,45 @@ fn get_value(c : char) -> u32 {
     }
 }
 
+fn part12(input: &str) -> (u32, u32) {
+    (
+        input
+            .split("\n")
+            .map(|line| {
+                let (first_half, second_half) = line.split_at(line.len() / 2);
+
+                let mut duplicates = String::new();
+
+                for c in first_half.chars() {
+                    if second_half.contains(c) && !duplicates.contains(c) {
+                        duplicates.push(c);
+                    }
+                }
+
+                duplicates.chars().map(get_value).sum::<u32>()
+            })
+            .sum(),
+        input
+            .split_whitespace()
+            .into_iter()
+            .array_chunks()
+            .map(|[l1, l2, l3]| {
+                l1.chars()
+                    .filter(|c| l2.contains(*c) && l3.contains(*c))
+                    .map(get_value)
+                    .take(1)
+                    .sum::<u32>()
+            })
+            .sum(),
+    )
+}
+
 fn main() {
-    let input = include_str!("input");
+    assert_eq!((157, 70), part12(include_str!("input_test")));
 
-    let mut sum1 : u32 = 0;
-    for line in input.split_whitespace() {
-        let (first_half, second_half) = line.split_at(line.len() / 2);
+    let (part1, part2) = part12(include_str!("input"));
 
-        let mut duplicates = String::new();
-
-        for c in first_half.chars() {
-            if second_half.contains(c) && !duplicates.contains(c) {
-                duplicates.push(c);
-            }
-        }
-
-        sum1 += duplicates.chars().map(get_value).sum::<u32>();
-    }
-
-    println!("Part 1: {}", sum1);
-
-    let mut sum2 : u32 = 0;
-    for [l1, l2, l3] in input.split_whitespace().array_chunks() {
-        sum2 += l1.chars().filter(|c| l2.contains(*c) && l3.contains(*c)).map(get_value).take(1).sum::<u32>();
-    }
-
-    println!("Part 2: {}", sum2);
+    println!("Day 03");
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
